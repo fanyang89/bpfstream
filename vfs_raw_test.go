b046da548a69464d8256717625c2016aa44555b7
fanyang <fanyang@smartx.com>
2025-06-24 11:00:23 +0800

    Update

--- vfs_raw_test.go
+++ vfs_raw_test.go
@@ -6,17 +6,14 @@ import (
 	"fmt"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/marcboeker/go-duckdb/v2"
+	"github.com/rs/zerolog/log"
 )
 
+// elapsed_ns=1104481628/4 rows=321039 ns_per_row=860 -> 1240694 ops
 func BenchmarkImportFromBpf(b *testing.B) {
-	r, err := os.Open("testdata/vfs-raw.ndjson")
-	if err != nil {
-		b.Fatal(err)
-	}
-	defer func() { _ = r.Close() }()
-
 	ctx := context.Background()
 	dsn := "bench.ddb"
 	tableName := "append_bench"
@@ -26,13 +23,14 @@ func BenchmarkImportFromBpf(b *testing.B) {
 		b.Fatal(err)
 	}
 
+	log.Info().Str("dsn", dsn).Msg("Connecting to db")
 	conn, err := connector.Connect(ctx)
 	if err != nil {
 		b.Fatal(err)
 	}
 
 	db := sql.OpenDB(connector)
-	defer func() { _ = db.Close() }()
+	log.Info().Msg("DB created")
 
 	_, err = db.Exec(fmt.Sprintf(dropTableSql, tableName))
 	if err != nil {
@@ -50,10 +48,24 @@ func BenchmarkImportFromBpf(b *testing.B) {
 	}
 	defer func() { _ = appender.Close() }()
 
+	now := time.Now()
+
 	for n := 0; n < b.N; n++ {
+		var r *os.File
+		r, err = os.Open("testdata/vfs-raw.ndjson")
+		if err != nil {
+			b.Fatal(err)
+		}
+
 		err = writeToDuckDB(r, appender.AppendRow)
 		if err != nil {
 			b.Fatal(err)
 		}
+
+		_ = appender.Flush()
+		_ = r.Close()
 	}
+
+	elapsed := time.Now().Sub(now)
+	log.Info().Int64("elapsed_ns", elapsed.Nanoseconds()).Int("n", b.N).Msg("Done in time")
 }
